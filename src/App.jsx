76212c6f019cea09fd0a9a106e992734bed97550
nischal119 <dhungeln12@gmail.com>
2023-08-02 18:18:55 +0545

    setup

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="main-div">
+      <div className="sub-div">
+        <img
+          src="https://cdn.siasat.com/wp-content/uploads/2021/01/1200-900-Facebook.jpg"
+          alt=""
+        />
+        <h1>Facebook</h1>
+        <a href="http://facebook.com" target="_blank">
+          Visit Site
+        </a>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+          obcaecati eaque culpa, quas sed voluptatum, consequatur, sunt
+          dignissimos quisquam delectus laudantium tempore consectetur dolor
+          harum dolore praesentium autem aperiam odio veritatis consequuntur
+          excepturi nesciunt animi? Perferendis voluptatibus accusantium iste!
+          Eveniet laboriosam officiis, iure officia repudiandae ipsa voluptatibs
+        </p>
+      </div>
+      <div className="sub-div instagram">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Instagram_logo_2022.svg/225px-Instagram_logo_2022.svg.png"
+          alt=""
+        />
+        <h1>Instagram</h1>
+        <a href="http://instagram.com" target="_blank">
+          Visit Site
+        </a>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Id,
+          doloribus nihil, qui ipsam neque consequuntur corrupti sint molestias
+          amet enim veritatis. Explicabo deleniti perferendis quibusdam tempora!
+          Ad, blanditiis! Atque dolor officia, in at nemo nesciunt molestias
+          eius, quos doloribus, similique neque id. Vero et natus quasi eius
+          fugit
+        </p>
+      </div>
+      <div className="sub-div tiktok">
+        <img
+          src="https://play-lh.googleusercontent.com/BmUViDVOKNJe0GYJe22hsr7juFndRVbvr1fGmHGXqHfJjNAXjd26bfuGRQpVrpJ6YbA=w240-h480-rw"
+          alt=""
+        />
+        <h1>Tiktok</h1>
+        <a href="http://tiktok.com" target="_blank">
+          Visit Site
+        </a>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Similique
+          obcaecati tenetur ad recusandae odio earum laboriosam modi iusto quod
+          aut nobis, magni est iste explicabo repellat accusantium consectetur
+          quia animi officia optio dicta adipisci. Consequatur ipsa nihil quam,
+          totam, pariatur inventore minima doloribus vero excepturi iste eveniet
+          illo quos error!
+        </p>
+      </div>
+      <div className="sub-div tiktok">
+        <img
+          src="https://seeklogo.com/images/T/twitter-x-logo-0339F999CF-seeklogo.com.png?v=638258077460000000"
+          alt=""
+        />
+        <h1>X</h1>
+        <a href="http://twitter.com" target="_blank">
+          Visit Site
+        </a>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima
+          consequuntur quasi consequatur perspiciatis facilis nostrum quisquam,
+          a, tenetur architecto dolore nesciunt quae eum laborum! Minus
+          provident iste quos tempora illo, fuga molestiae voluptatem quaerat
+          vel possimus hic autem nesciunt veniam eaque iusto, velit nisi. Id
+          perspiciatis minus accusamus quibusdam ab?
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
